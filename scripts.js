6ce4d7e6bb88fc79f41cff4cddca245b2e910d64
Andrew Pomerleau <arpomerleau@gmail.com>
2022-06-05 13:52:40 -0400

    Added land button functionality

--- scripts.js
+++ scripts.js
@@ -2,21 +2,29 @@
 // Remember to pay attention to page loading!
 
 function init() {
-    const takeOffConfirm = "Confirm that the shuttle is ready for takeoff.";
     const shuttleBackground = document.getElementById("shuttleBackground");
     const flightStatus = document.getElementById("flightStatus");
     const takeOffButton = document.getElementById("takeoff");
+    const landButton = document.getElementById("landing");
     const spaceShuttleHeight = document.getElementById("spaceShuttleHeight");
 
     function takeOff() {
-        if (confirm(takeOffConfirm)) {
+        if (confirm("Confirm that the shuttle is ready for takeoff.")) {
             flightStatus.innerHTML = "Shuttle in flight.";
             shuttleBackground.style.backgroundColor = "blue";
             spaceShuttleHeight.innerHTML = "10,000";
         }
     }
 
+    function land() {
+        alert("The shuttle is landing. Landing gear engaged.");
+        flightStatus.innerHTML = "The shuttle has landed.";
+        shuttleBackground.style.backgroundColor = "green";
+        spaceShuttleHeight.innerHTML = "0";
+    }
+
     takeOffButton.addEventListener("click", takeOff);
+    landButton.addEventListener("click", land);
 }
 
 window.addEventListener("load", init);
