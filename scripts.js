bde927f8ed760ced285b873d3f8bd4f17172cb95
Andrew Pomerleau <arpomerleau@gmail.com>
2022-06-05 13:45:28 -0400

    Added takeoff button functionality

--- scripts.js
+++ scripts.js
@@ -2,7 +2,21 @@
 // Remember to pay attention to page loading!
 
 function init() {
-    console.log("Working...");
+    const takeOffConfirm = "Confirm that the shuttle is ready for takeoff.";
+    const shuttleBackground = document.getElementById("shuttleBackground");
+    const flightStatus = document.getElementById("flightStatus");
+    const takeOffButton = document.getElementById("takeoff");
+    const spaceShuttleHeight = document.getElementById("spaceShuttleHeight");
+
+    function takeOff() {
+        if (confirm(takeOffConfirm)) {
+            flightStatus.innerHTML = "Shuttle in flight.";
+            shuttleBackground.style.backgroundColor = "blue";
+            spaceShuttleHeight.innerHTML = "10,000";
+        }
+    }
+
+    takeOffButton.addEventListener("click", takeOff);
 }
 
-window.onload = init();
+window.addEventListener("load", init);
